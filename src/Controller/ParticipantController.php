fc1540728702f240590b19aaa5f9a1fa3a5ae3c9
AlexGit-ENI <alexis.joignant2026@campus-eni.fr>
2026-05-19 16:59:05 +0200

    Page Utilisateur non terminée

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -2,34 +2,62 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Form\ParticipantType;
+use App\Repository\ParticipantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 final class ParticipantController extends AbstractController
 {
     #[Route('/participant', name: 'app_participant')]
-    public function index(): Response
+    public function new(
+        Request $request,
+        ParticipantRepository $participantRepository,
+        UserPasswordHasherInterface $passwordHasher,
+    ) : Response
     {
-        return $this->render('participant/index.html.twig', [
-            'controller_name' => 'ParticipantController',
-        ]);
-    }
-    #[Route('/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        // Si déjà connecté, on redirige
-        if ($this->getUser()) {
-            return $this->redirectToRoute('sorties');
+        $participant = new Participant();
+        $form = $this->createForm(ParticipantType::class, $participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $passwordHasher->hashPassword($participant, $form->get('plainPassword')->getData());
+            $hashedPassword = $passwordHasher->hashPassword($participant, $plainPassword);
+            $participant->setPassword($hashedPassword);
+
+            $participantRepository->save($participant, true);
+            $this->addFlash('Changement enregistré.');
+            return $this->redirectToRoute('app_participant');
         }
 
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastAuthenticationError();
-
-        return $this->render('participant/login.html.twig', [
-            'error' => $error,
-            'last_username' => $lastUsername,
+        return $this->render('participant/index.html.twig', [
+            'form' => $form
         ]);
-}
+    }
+//
+//        return $this->render('participant/index.html.twig', [
+//            'controller_name' => 'ParticipantController',
+//        ]);
+//    }
+//    #[Route('/login', name: 'app_login')]
+//    public function login(AuthenticationUtils $authenticationUtils): Response
+//    {
+//        // Si déjà connecté, on redirige
+//        if ($this->getUser()) {
+//            return $this->redirectToRoute('sorties');
+//        }
+//
+//        $error = $authenticationUtils->getLastAuthenticationError();
+//        $lastUsername = $authenticationUtils->getLastAuthenticationError();
+//
+//        return $this->render('participant/login.html.twig', [
+//            'error' => $error,
+//            'last_username' => $lastUsername,
+//        ]);
+//}
 }
