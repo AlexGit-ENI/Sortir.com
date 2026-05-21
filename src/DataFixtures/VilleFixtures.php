12d18875c5af6d07e9703f4139bc4598b646caa5
YannBlev <36kbrl@gmail.com>
2026-05-21 16:18:20 +0200

    debug Fixtures

--- src/DataFixtures/VilleFixtures.php
+++ src/DataFixtures/VilleFixtures.php
@@ -8,45 +8,24 @@ use Doctrine\Persistence\ObjectManager;
 
 class VilleFixtures extends Fixture
 {
-    public static array $villeKeys = [
-        'Quimper',
-        'Rennes',
-        'Vannes',
-        'Nantes'
-    ];
+    public static array $villeKeys = [];
 
     public function load(ObjectManager $manager): void
     {
+        $faker = \Faker\Factory::create('fr_FR');
+
         // $product = new Product();
         // $manager->persist($product);
 
-        $quimper = new Ville();
-        $quimper->setNom('Quimper');
-        $quimper->setCodePostal('29000');
+        for ($i = 0; $i <= 10; $i++) {
+            $ville = new Ville();
+            $ville->setNom($faker->city());
+            $ville->setCodePostal($faker->postcode());
+            static::$villeKeys[$i] = $ville->getNom();
+            $this->addReference(static::$villeKeys[$i], $ville);
+            $manager->persist($ville);
+        }
 
-        $this->addReference(static::$villeKeys[0], $quimper);
-        $manager->persist($quimper);
-
-        $rennes = new Ville();
-        $rennes->setNom('Rennes');
-        $rennes->setCodePostal('35000');
-
-        $this->addReference(static::$villeKeys[1], $rennes);
-        $manager->persist($rennes);
-
-        $vannes = new Ville();
-        $vannes->setNom('Vannes');
-        $vannes->setCodePostal('56000');
-
-        $this->addReference(static::$villeKeys[2], $vannes);
-        $manager->persist($vannes);
-
-        $nantes = new Ville();
-        $nantes->setNom('Nantes');
-        $nantes->setCodePostal('44000');
-
-        $this->addReference(static::$villeKeys[3], $nantes);
-        $manager->persist($nantes);
 
         $manager->flush();
     }
