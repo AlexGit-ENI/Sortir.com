4042bf939885351144c0cfe85c74b34212cbd255
YannBlev <36kbrl@gmail.com>
2026-05-28 12:44:29 +0200

    [feature] css detail participant

--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -7,8 +7,10 @@ use App\Entity\Site;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
@@ -20,21 +22,77 @@ class ParticipantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('prenom')
-            ->add('nom')
-            ->add('telephone')
-            ->add('mail')
+            ->add('username', TextType::class, [
+                'label' => 'Pseudo',
+                'label_attr' => [
+                    'class' => 'form-label',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+                'label_attr' => [
+                    'class' => 'form-label',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+                'label_attr' => [
+                    'class' => 'form-label',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('telephone', TextType::class, [
+                'label' => 'Téléphone',
+                'label_attr' => [
+                    'class' => 'form-label',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('mail', EmailType::class, [
+                'label' => 'E-mail',
+                'label_attr' => [
+                    'class' => 'form-label',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
-                'first_options'  => ['label' => 'Password', 'hash_property_path' => 'password'],
-                'second_options' => ['label' => 'Repeat Password'],
+                'first_options'  => ['label' => 'Mot de passe', 'hash_property_path' => 'password',                 'label_attr' => [
+                    'class' => 'form-label',
+                ],
+                    'attr' => [
+                        'class' => 'form-control',
+                    ]],
+                'second_options' => ['label' => 'Confirmation du mot de passe',                 'label_attr' => [
+                    'class' => 'form-label',
+                ],
+                    'attr' => [
+                        'class' => 'form-control',
+                    ]],
                 'mapped' => false,
             ])
             ->add('site', EntityType::class, [
                 'class' => Site::class,
                 'choice_label' => 'nom',
                 'multiple' => false,
+                'label_attr' => [
+                    'class' => 'form-label',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ]
             ]);
 
     }
